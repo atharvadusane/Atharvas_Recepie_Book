ae5a01e0dbab313e2bc3acc747eedea2e94cd94c
atharvadusane <atharva.dusane21@pccoepune.org>
2023-08-11 00:26:33 +0530

    responsiveness added

--- recipie.js
+++ recipie.js
@@ -9,9 +9,10 @@ const ingredients=document.querySelector('.ingredients')
 const measures=document.querySelector('.measures')
 const recepie_instructions=document.querySelector('.recepie-instructions')
 
+const pending=true
 
 searchBut.addEventListener('click',(e)=>{
-    // recepieSection.innerHTML=``
+    // recepieSection.innerHTML={}
     e.preventDefault();
     fetchAPI(inpBox.value.trim())
     // inpBox.value=""
@@ -20,24 +21,25 @@ searchBut.addEventListener('click',(e)=>{
 })
 // console.log(mealsInfo)
 
-
-const fetchAPI=async(param)=>{    
+const fetchAPI=async(param)=>{ 
+    recepieSection.innerHTML="<h2>Fetching recepies...</h2>"
     const data= await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${param}`)
     const response= await data.json()
-    // console.log(response.meals[0])
+    // console.log(response.meals)
+    recepieSection.innerHTML=""
     response.meals.forEach(meal => {
-
         let mealsInfo= document.createElement('div')
         mealsInfo.classList='mealsInfo'
         recepieSection.appendChild(mealsInfo)
         mealsInfo.innerHTML=`
-        <img class="mealImage" src="${meal.strMealThumb}">
-        <p></p><span class="mealName">${meal.strMeal}</span>
+        <img class="mealImage" src="${meal.strMealThumb}"> 
+        <span class="mealName">${meal.strMeal}</span>
         <span class="mealArea">${meal.strArea}</span>
         <span class="mealCategory">${meal.strCategory}</span>
         `
         // console.log(meals.strMealThumb)
         mealsInfo.addEventListener('click',function addinstructions(){
+            // append meal.strInstructions only if the " instructions " area is `empty` else empty it first and then add meal.strIngredients
             if(instructions.innerHTML=``){
                 recepie_details.style.display='block'
                 // console.log(meal.strInstructions)
@@ -54,32 +56,30 @@ const fetchAPI=async(param)=>{
             instructions.innerHTML=``
         })
         ingredients.addEventListener('click',()=>{
-            if(ingredients.textContent="Ingredients"){
+            if(ingredients.innerHTML="Ingredients"){
                 // fetchIngList(meal);
-                ingredients.textContent="Back to instructions"
                 dispIngredients(meal)
-            }else{
-                ingredients.innerHTML=`
-                <span>Ingredients</span>
-                `
+                ingredients.innerHTML="Back to instructions"
+
+            }else if(ingredients.innerHTML.includes="to"){
+                // ingredients.innerHTML=`
+                // <span>Ingredients</span>
+                // `
+                ingredients.innerHTML="Ingredients"
             }
         })
-    });
-    
+    }); 
 }
 
 // instruction, ingredients
-
-
 
 const fetchIngList=(meal)=>{
     let ingredient_list="";
     console.log(meal);
     for(let i=1;i<=20;i++){
         const ingredient=meal[`strIngredient${i}`]
-        
-// *** You can access the properties of an object in JavaScript in 3 ways:
 
+// *** You can access the properties of an object in JavaScript in 3 ways:
 //     Dot property accessor: object.property
 //     Square brackets property accessor: object['property']
 //     Object destructuring: const { property } = object
